e3bcc3c1488bc9b195a17b8f75ceff182f8d0125
DucAnh2111 <vuducanh2103@gmail.com>
2018-09-21 21:26:47 +0700

    update ss4

---
+++ session4/for_explain.py
@@ -0,0 +1,2 @@
+for i in [0, 1, 2, 7]:
+    print(i)
---
+++ session4/turtle_intro.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+shape("turtle")
+color("teal")
+speed(0)
+for i in range(30):
+    forward(100)
+    left(90)
+for i in range(3):
+    forward(100)
+    left(90)
+
+    left(30)
+
+mainloop()
