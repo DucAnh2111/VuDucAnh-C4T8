b9180c01ab6b55a91f9e9cacc42fe190b2465df0
DucAnh2111 <vuducanh2103@gmail.com>
2018-10-19 21:00:05 +0700

    BT

---
+++ session10.py/Bai1.py
@@ -0,0 +1,32 @@
+# Câu a
+# a = "*"
+# b = 20
+# c = a*b
+# print (c)
+
+# Câu b
+# a = "*"
+# b = input("Number")
+# c = a*int(b)
+# print(c)
+
+# Câu c
+# a = "*"
+# b = "x"
+# for i in range (8):
+#     if i % 2 == 0:
+#         print (a,end= '')
+#     else:
+#         print (b,end='')
+
+# Câu d
+# a = "*"
+# b = "x"
+# c = int(input("Number"))
+# for i in range (c):
+#       if i % 2 == 0:
+#           print (a,end= '')
+#       else:
+#           print (b,end= '') 
+
+Câu e
